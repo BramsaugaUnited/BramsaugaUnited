c98d78ed814cda62bcee4eca4915b3795829f190
PritamSapkota <slicefrost1@gmail.com>
2023-07-12 11:55:08 -0400

    update navbar avoid reload

--- app.js
+++ app.js
@@ -6,20 +6,11 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
 
+$('.toggle-button').click(function(e){
+  e.preventDefault()
+})
+
 
 // NavBar Sticky Scroll Effect
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 
-// Slide Show Container For News 
